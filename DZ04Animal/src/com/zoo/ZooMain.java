1a587659cfb30bea7ad6fc4f6127bf888f185ad6
RomanAkhmedshin <detector268@gmail.com>
2021-05-08 11:30:26 +0300

    test1

---
+++ DZ04Animal/src/com/zoo/ZooMain.java
@@ -0,0 +1,11 @@
+package com.zoo;
+
+import com.animal.Animal;
+
+public class ZooMain {
+    public static void main(String[] args) {
+        System.out.println("1");
+        Animal pig = new Animal();
+        System.out.println(pig.name);
+    }
+}
